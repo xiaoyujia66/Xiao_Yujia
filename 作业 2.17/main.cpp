f9d1d6e000060ea9ba868d900e48fab0e4a2cad0
xiaoyujia66 <435713285@qq.com>
2019-03-11 16:40:31 +0800

    C++作业  测绘4班肖雨佳

---
+++ 作业 2.17/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "1 2 3 4" << endl;
+
+    std::cout << "1" << "2" << "3" << "4" << endl;
+
+    std::cout << "1";
+    std::cout << "2";
+    std::cout << "3";
+    std::cout << "4" << endl;
+}
